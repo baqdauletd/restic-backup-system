551bc0a4a3f455e77934c4c5ee3072a99d7f8ead
baqdauletd <baqdaulet.dauletov04@gmail.com>
2025-07-10 16:55:20 +0500

    backup logic and main

---
+++ logic/backup.go
@@ -0,0 +1,132 @@
+package logic
+
+import (
+	// "compress/gzip"
+	"context"
+	// "crypto/aes"
+	// "crypto/cipher"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/minio/minio-go/v7"
+	// "github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+const (
+	chunkSize = 4 * 1024 * 1024 // 4MB
+	bucketChunks = "chunks"
+	bucketSnapshots = "snapshots"
+)
+
+var encryptionKey = []byte("0123456789abcdef0123456789abcdef") // 32 bytes (for the first try)
+
+type FileEntry struct {
+	Path   string   `json:"path"`
+	Chunks []string `json:"chunks"`
+}
+
+type Snapshot struct {
+	Timestamp time.Time   `json:"timestamp"`
+	Files     []FileEntry `json:"files"`
+}
+
+func Backup(minioClient *minio.Client, sourceDir string) error {
+	var snapshot Snapshot
+	snapshot.Timestamp = time.Now()
+
+	err := filepath.WalkDir(sourceDir, func(path string, d os.DirEntry, err error) error {
+		if err != nil || d.IsDir() {
+			return err
+		}
+
+		relPath, _ := filepath.Rel(sourceDir, path)
+		fmt.Println("Backing up:", relPath)
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		var chunks []string
+		buf := make([]byte, chunkSize)
+
+		for {
+			n, err := file.Read(buf)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
+
+			chunk := buf[:n]
+			hash := sha256.Sum256(chunk)
+			chunkID := hex.EncodeToString(hash[:])
+
+			// eheck if chunk already exists
+			found, _ := minioClient.StatObject(context.Background(), bucketChunks, chunkID, minio.StatObjectOptions{})
+			if found.Size == 0 {
+				// encrypt and compress
+				encChunk, err := encryptAndCompress(chunk)
+				if err != nil {
+					return err
+				}
+
+				// upload to S3
+				_, err = minioClient.PutObject(
+					context.Background(),
+					bucketChunks,
+					chunkID,
+					encChunk,
+					-1,
+					minio.PutObjectOptions{},
+				)
+				if err != nil {
+					return err
+				}
+			}
+
+			chunks = append(chunks, chunkID)
+		}
+
+		snapshot.Files = append(snapshot.Files, FileEntry{
+			Path:   relPath,
+			Chunks: chunks,
+		})
+
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// save snapshots in json
+	snapData, err := json.MarshalIndent(snapshot, "", "  ")
+	if err != nil {
+		return err
+	}
+	snapName := fmt.Sprintf("snap-%d.json", time.Now().Unix())
+	_, err = minioClient.PutObject(
+		context.Background(),
+		bucketSnapshots,
+		snapName,
+		strings.NewReader(string(snapData)),
+		int64(len(snapData)),
+		minio.PutObjectOptions{ContentType: "application/json"},
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("✅ Snapshot saved as", snapName)
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"restic-backup-system/logic"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func main() {
+	endpoint := "localhost:9000"
+	accessKey := "miniobaga"
+	secretKey := "miniobaga"
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
+		Secure: false,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// chunks bucket
+	err = minioClient.MakeBucket(context.Background(), "chunks", minio.MakeBucketOptions{})
+	if err != nil {
+		fmt.Println("chunks bucket exists or error:", err)
+	}
+
+	// snapshots bucket
+	err = minioClient.MakeBucket(context.Background(), "snapshots", minio.MakeBucketOptions{})
+	if err != nil {
+		fmt.Println("snapshots bucket exists or error:", err)
+	}
+
+	fmt.Println("Buckets created or already exist")
+
+	err = logic.Backup(minioClient, "./my-folder")
+	if err != nil {
+		panic(err)
+	}
+}
